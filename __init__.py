1b171cc125224d57805482739252d5d0b0694fef
abhinav <me@abhnv.xyz>
2024-08-21 03:31:02 +0530

    Change name

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ NODE_CLASS_MAPPINGS = {
     "Load Json File": UtilLoadJson,
 }
     
-print("\033[34mComfyUI Abhi Nodes: \033[92mLoaded\033[0m")
+print("\033[34mComfyUI Nodes Abhi: \033[92mLoaded\033[0m")
