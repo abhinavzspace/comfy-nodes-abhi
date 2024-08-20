cd1a802150f925b4fbccbc3c3d5b7d3524fb7696
abhinav <me@abhnv.xyz>
2024-08-21 03:21:33 +0530

    initial commit

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .nodes.nodes import *
+
+NODE_CLASS_MAPPINGS = { 
+    "Load Json File": UtilLoadJson,
+}
+    
+print("\033[34mComfyUI Abhi Nodes: \033[92mLoaded\033[0m")
---
+++ nodes/nodes.py
@@ -0,0 +1,26 @@
+import json
+
+class UtilLoadJson:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "file_path": ("STRING", {"default": ""}),
+            },
+            "optional": {
+                "print_to_console": ("BOOLEAN", {"default": False}),
+            },
+        }
+
+    RETURN_TYPES = ("JSON",)
+    CATEGORY = "Abhi Nodes/Utils"
+    FUNCTION = "load_json"
+
+    def load_json(self, file_path: str, print_to_console=False):
+        # Open and read the JSON file directly
+        with file_path.open("r") as file:
+            data = json.load(file)
+            if print_to_console:
+                print("JSON content:", json.dumps(data, indent=4))
+
+            return (data,)
